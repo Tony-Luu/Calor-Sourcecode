50bc7c41b678d541c2da2370234c743ad7a9f3ac
Tony Luu <tonyluu@hotmail.co.uk>
2021-05-14 18:50:52 +0100

    Scripts from Calor project

---
+++ TL_VideoPlayerURL.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Video;
+
+public class TL_VideoPlayerURL : MonoBehaviour
+{
+    public string VideoClipName;
+    public VideoPlayer Video;
+
+
+    void Awake()
+    {
+        Video.url = System.IO.Path.Combine(Application.streamingAssetsPath, VideoClipName + ".mp4");
+        Video.Play();
+    }
+
+}
